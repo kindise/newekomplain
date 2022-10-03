5f7d799d338db8963c220633bd90d6ea3d420f1d
Muhammad Wildanul Falah <wildan.gant12@gmail.com>
2022-10-03 10:49:48 +0700

    refresh ticket

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -74,4 +74,23 @@ class HomeController extends Controller
             'data'
         ]));
     }
+
+    public function refreshTicket()
+    {
+        $sql = "SELECT
+            a.id,
+            A.created_at tgl_tiket, A.nama, B.cnmunit, A.description, A.solution, A.pic, C.name as assign,
+            (select D.updated_at from ticket_status D where A.id=D.ticket_id order by D.updated_at desc limit 1) as tgl_selesai
+            FROM tickets A
+            JOIN msunit B on A.ckdunit=B.ckdunit
+            JOIN users C on A.assignto=C.id
+            WHERE A.created_at BETWEEN '2022-06-01 00:00:00' AND '2022-09-30 23:59:59'
+        ORDER BY A.created_at asc";
+
+        $data = DB::select($sql);
+
+        return view('refresh', compact([
+            'data'
+        ]));
+    }
 }
