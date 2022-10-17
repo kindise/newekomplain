6e6234287e5385cf2aee3b77d64b90cd8ff2a36a
Muhammad Wildanul Falah <wildan.gant12@gmail.com>
2022-10-17 09:05:32 +0700

    form finish

--- resources/views/finish.blade.php
+++ resources/views/finish.blade.php
@@ -18,6 +18,29 @@
                     <form method="POST" action="{{ route('doneticket') }}">
                         @csrf
                         <div class="mb-3 row">
+                            <label for="solusi" class="col-sm-4 col-form-label text-md-end">Tanggal / Jam Selesai</label>
+                            <div class="col-sm-6">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <input type="date" class="form-control" name="tgl_selesai" id="tgl_selesai" max="{{ date('Y-m-d') }}" value="{{ empty(old('tgl_selesai')) ? date('Y-m-d') : old('tgl_selesai') }}">
+                                        @error('tgl_selesai')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="time" class="form-control" name="jam_selesai" id="jam_selesai" value="{{ empty(old('jam_selesai')) ? date('H:i') : old('jam_selesai') }}">
+                                        @error('jam_selesai')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
                             <label for="solusi" class="col-sm-4 col-form-label text-md-end">{{ __('Solusi') }}</label>
                             <div class="col-sm-6">
                                 <textarea class="form-control @error('solusi') is-invalid @enderror" id="solusi" name="solusi" rows="3" required>{{ old('solusi') }}</textarea>
